600b09cc7ae13a70a0f8429ea2a97892fa1aebf8
isabeela <31148780+isabeela@users.noreply.github.com>
2021-10-25 13:56:01 -0300

    menu
    menu

--- livros/src/main/java/com/example/livros/LivrosController.java
+++ livros/src/main/java/com/example/livros/LivrosController.java
@@ -10,5 +10,25 @@ public class LivrosController {
 	public String principal () {
 		return "index";
 	}
+	
+	@GetMapping("/lidos")
+	public String lidos () {
+		return "lidos";
+	}
+	
+	@GetMapping("/proximas")
+	public String proximas () {
+		return "proximas";
+	}
+	
+	@GetMapping("/lista-desejos")
+	public String listadeDesejos () {
+		return "lista-desejos";
+	}
+		
+	@GetMapping("/emprestados")
+	public String emprestados () {
+		return "emprestados";
+	}
 
 }
